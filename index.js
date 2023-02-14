60e430ea86d22e22a2cfce2411a6b430d48d5dd2
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 07:59:48 +0200

    Task Model

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", true);
 
 async function main() {
-  await connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
 main()
