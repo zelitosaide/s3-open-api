71b25ef4d601b3b31deeb7a4b6648658a3dbcdcd
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 07:19:24 +0200

    Task Model

---
+++ models/task/task.js
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    text: { type: String },
+    done: { type: Boolean },
+  },
+  { timestamps: true }
+);
+
+export const Task = model("Task", taskSchema);
