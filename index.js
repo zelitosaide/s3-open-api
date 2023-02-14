071bde9bb1651415844e8fa6a5e0fb4a680b1c90
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 08:33:35 +0200

    Task Model

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function main() {
   const kittySchema = new mongoose.Schema({ name: String });
   const Kitten = mongoose.model("Kitten", kittySchema);
 
-  const silence = new Kitten({ name: "silence" });
+  const silence = new Kitten({ name: "Silence" });
 }
 
 main()
