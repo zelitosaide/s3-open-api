7f6b027d6a29af1bd4faa2b2d78fef8b3303c208
zelitosaide <zelitosaide83@gmail.com>
2023-02-14 08:40:10 +0200

    Task Model

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ mongoose.set("strictQuery", true);
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/test");
   const kittySchema = new mongoose.Schema({ name: String });
+  kittySchema.methods.speak = function () {
+    const greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  };
+
   const Kitten = mongoose.model("Kitten", kittySchema);
 
   const silence = new Kitten({ name: "Silence" });
